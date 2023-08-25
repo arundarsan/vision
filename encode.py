f7c73361ddf5f2a384211db16bc4407fb23e5e80
arun <arun.d@fayausa.com>
2023-08-25 14:14:47 +0100

    change name to participant

--- encode.py
+++ encode.py
@@ -2,51 +2,30 @@ import os
 import pickle
 import face_recognition
 import cv2
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
-from firebase_admin import storage
+from firebase_admin import *
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred , {
     'databaseURL' : 'https://face-recognition-36ef1-default-rtdb.firebaseio.com',
     'storageBucket' : 'face-recognition-36ef1.appspot.com'
     })
-studentfolderPath = 'resources'
-studentknownPersonListPath = os.listdir(studentfolderPath)
-print(studentknownPersonListPath)
-studentknownPersonList = []
-studentIds = []
-for path in studentknownPersonListPath:
-   studentknownPersonList.append(cv2.imread(os.path.join(studentfolderPath,path)))
-   studentIds.append(os.path.splitext(path)[0])
-#print(studentknownPersonList)
-#print(studentIds)
-   fileName = f'{studentfolderPath}/{path}'
-   bucket = storage.bucket()
-   blob = bucket.blob(fileName)
-   blob.upload_from_filename(fileName)
-def findencording(studentknownPersonList):
+participantfolderPath = 'resources'
+participantknownPersonListPath = os.listdir(participantfolderPath)
+participantknownPersonList = []
+participantIds = []
+for path in participantknownPersonListPath:
+   participantknownPersonList.append(cv2.imread(os.path.join(participantfolderPath,path)))
+   participantIds.append(os.path.splitext(path)[0])
+def findencording(participantknownPersonList):
     encodeList = []
-    print(encodeList)
-    for img in studentknownPersonList:
+    for img in participantknownPersonList:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-       # print(img)
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
-        print(encodeList)
     return encodeList
-
-
-
 print("Start Encording.......")
-print(studentknownPersonList)
-encodeListKnown = findencording(studentknownPersonList)
+encodeListKnown = findencording(participantknownPersonList)
 print("Complete Encording......5")
-print(encodeListKnown)
-
-
-encodeListKnownWithIds = [encodeListKnown,studentIds ]
-
+encodeListKnownWithIds = [encodeListKnown,participantIds ]
 file  = open("encoding.p",'wb')
 pickle.dump(encodeListKnownWithIds, file)
 file.close()
